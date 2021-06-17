bf27af9f2e1573407c484c44a6d7b49d1cd71687
Nitin <nbnitin.bhatia@gmail.com>
2021-06-17 16:20:16 +0530

    conditionally open new and old view on button click...

--- CustomMusicPlayer/ViewController.swift
+++ CustomMusicPlayer/ViewController.swift
@@ -31,20 +31,50 @@ class ViewController: UIViewController {
         //for mp4 videos
        // http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4
         
-        performPlay(url:URL(string: "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8")!)
+        performPlay(url:URL(string: "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8")!,isViewOld: false)
     }
     @IBAction func playAudio(_ sender: Any) {
-        performPlay(url:URL(string: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")!)
+        performPlay(url:URL(string: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")!,isViewOld: false)
     }
     
-    private func performPlay(url:URL){
+    @IBAction func playVideoInOldView(_ sender: Any) {
+        performPlay(url:URL(string: "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8")!,isViewOld: true)
+    }
+    
+    @IBAction func playAudioInOldView(_ sender: Any) {
+        performPlay(url:URL(string: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")!,isViewOld: true)
+    }
+    
+    private func performPlay(url:URL,isViewOld:Bool){
         let sb = UIStoryboard(name: "Player", bundle: nil)
-        let vc = sb.instantiateInitialViewController() as? AudioViewControllerNewView
+        
+        if isViewOld {
+            presentOldView(sb: sb, url: url)
+        } else {
+            presentNewView(sb: sb, url: url)
+        }
+        
+    }
+    
+    private func presentNewView(sb:UIStoryboard,url:URL){
+        let vc = sb.instantiateViewController(withIdentifier: "AudioViewControllerNewView") as? AudioViewControllerNewView
         //http://techslides.com/demos/sample-videos/small.mp4
         //http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4
         //https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3
         vc?.urlToPlay = url
         AppUtility.lockOrientation(.landscape, andRotateTo: .landscapeLeft)
+        vc?.modalPresentationStyle = .fullScreen
+        self.present(vc!, animated: true, completion: nil)
+    }
+    
+    private func presentOldView(sb:UIStoryboard,url:URL){
+        let vc = sb.instantiateViewController(identifier: "AudioViewController") as? AudioViewController
+        //http://techslides.com/demos/sample-videos/small.mp4
+        //http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4
+        //https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3
+        vc?.urlToPlay = url
+        AppUtility.lockOrientation(.landscape, andRotateTo: .landscapeLeft)
+        vc?.modalPresentationStyle = .fullScreen
         self.present(vc!, animated: true, completion: nil)
     }
 }
